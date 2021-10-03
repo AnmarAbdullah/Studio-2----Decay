5f591e72fdfae55c899cf3aef1097cba4680fd28
Jeff <j.taylor@student.saedubai.com>
2021-10-03 18:52:57 +0400

    Prototype update
    added a bunch of gun play elements like reloading, gun switching, enemy health, gundamage, particle effects

--- Assets/Scripts/Jeff/Gun.cs
+++ Assets/Scripts/Jeff/Gun.cs
@@ -1,26 +1,80 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float impactForce = 100f;
+    public float fireRate = 15f;
+
+    public int maxAmmo = 10;
+    int currentAmmoAmount = -1;
+    public float reloadTime = 1f;
+    bool isReloading = false;
+
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    float nextTimeToFire = 0f;
+
+    public Animator animator;
+
+    private void Start()
+    {
+        if (currentAmmoAmount == -1)
+            currentAmmoAmount = maxAmmo;
+    }
+
+    private void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (isReloading)
+            return;
+
+        if ((Input.GetKeyDown(KeyCode.R) && currentAmmoAmount < maxAmmo) || currentAmmoAmount <= 0)
         {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("reloading");
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+
+        animator.SetBool("Reloading", false);
+
+        yield return new WaitForSeconds(.25f);
+
+        currentAmmoAmount = maxAmmo;
+        isReloading = false;
+    }
     void Shoot()
     {
+        currentAmmoAmount--;
+
         muzzleFlash.Play();
+
         RaycastHit hit;
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
 
@@ -29,6 +83,15 @@ public class Gun : MonoBehaviour
             {
                 target.TakeDamage(damage);
             }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+
+            Destroy(impactGO, 2f);
         }
     }
 }
