fb8fc3ef78b7893d0c266402487bfb4afcdace8e
MigatteNoGokuii <anmar_2101@hotmail.com>
2021-10-28 18:45:46 +0400

    Update 1.3.9
    - Added Abilities Display UI
    - Added Abilities Counter UI
    - Added Player health UI
    - Added Weapons UI
    - Added Bomber Enemy
    - Added Screamer Enemy
    - Fixed UI Null Error

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -3,22 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FourPlatformUI : MonoBehaviour
+public class PlayerUI : MonoBehaviour
 {
     float currentMeter;
     float MAXMeter = 10000;
+    float currentHealth;
+    float MaxHealth = 300;
+
     public Image MeterUI;
+    public Image HealthUI;
+   
+    PlayerController player;
     FourPlatformsTask meter;
+   // GunSwitcher switcher;
+
 
     void Start()
     {
         MeterUI = GetComponent<Image>();
+        HealthUI = GetComponent<Image>();
+
+       // switcher = FindObjectOfType<GunSwitcher>();
+
         meter = FindObjectOfType<FourPlatformsTask>();
+        player = FindObjectOfType<PlayerController>();
     }
 
     void Update()
     {
         currentMeter = meter.Meter;
         MeterUI.fillAmount = currentMeter / MAXMeter;
+
+        currentHealth = player.Health;
+        HealthUI.fillAmount = currentHealth / MaxHealth;
     }
 }
