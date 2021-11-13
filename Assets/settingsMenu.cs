dfe140d552d9495dbff4ebbaf57b739c658e1fc8
ghosthulk <ghosthulk29@gmail.com>
2021-11-13 12:20:17 +0400

    update 1.6.1
    settings menu

---
+++ Assets/settingsMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class settingsMenu : MonoBehaviour
+{
+    public AudioMixer audiomixer;
+    Resolution[] resolutions;
+    public Dropdown resolutionDropDown;
+     void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropDown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex =0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option resolutions[i].width  "x" * resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        
+
+        }
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
+
+    }
+    public void SetResolution( int resoltuionIndex)
+    {
+        Resolution resolution = resolution(resoltuionIndex);
+        Screen.SetResolution(resolution.width, resolution.height,Screen.fullScreen);
+    }
+   
+    public void SetVolume(float Volume)
+    {
+        audiomixer.SetFloat("volumeParam", Volume);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
