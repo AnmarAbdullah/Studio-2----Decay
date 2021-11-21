e22a613fdb40f790e8a05a7253cec4e73d22e5f1
MigatteNoGokuii <anmar_2101@hotmail.com>
2021-11-21 15:02:47 +0400

    Project Fix

--- Assets/Scripts/Challenges/DefendObject.cs
+++ Assets/Scripts/Challenges/DefendObject.cs
@@ -1,14 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefendObject : MonoBehaviour
 {
-    public float health;
-
-    //if object hit by enemies here
-    private void OnCollisionEnter(Collision collision)
+    public float health = 10000;
+    public GameObject healthUI;
+    public Image healthbar;
+    PlayerController player;
+    void Start()
     {
-        
+        this.player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+    }
+    void Update()
+    {
+        healthbar.fillAmount = health / 10000;
+        if(player.ChallengeIndex == 2)
+        {
+            healthUI.SetActive(true);
+        }
+        else { healthUI.SetActive(false); }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Zombie"))
+        {
+            health -= 20f;
+        }
     }
 }
--- Assets/Scripts/Player & Guns/GunController.cs
+++ Assets/Scripts/Player & Guns/GunController.cs
@@ -77,7 +77,7 @@ public class GunController : MonoBehaviour
         if (isReloading)
             return;
 
-
+        reservedAmmoCapacity = ammoInReserve;
         if (Input.GetMouseButton(0) && canShoot && ammoInClip > 0)
         {
             canShoot = false;
@@ -87,12 +87,13 @@ public class GunController : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.R) && ammoInClip < clipSize && ammoInReserve > 0 || ammoInClip <= 0)
         {
             StartCoroutine(Reload());
-
+            
             int amountNeeded = clipSize - ammoInClip;
             if (amountNeeded >= ammoInReserve)
             {
                 ammoInClip += ammoInReserve;
-                ammoInReserve -= amountNeeded;
+                ammoInReserve = 0 ;
+
             }
             else
             {
