da22aa8d3bb7ba42a54cb22b5a5154877154c0c7
ghosthulk <ghosthulk29@gmail.com>
2021-11-26 18:31:36 +0400

    update 1.8.5
    pause menu  (need testing) and removed unnecessary assets

---
+++ Assets/Scripts/UI & Music/pauseMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    public GameObject Pausemenu;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        Pausemenu.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    public void Pause()
+    {
+        Pausemenu.SetActive(true);
+        Time.timeScale = 0f; // freezes time in pause
+        IsPaused = true;
+    }
+
+
+}
